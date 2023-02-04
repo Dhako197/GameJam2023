1c009cab09ff15b31c4b39b239a422a68aca1da8
Dhako197 <53204420+Dhako197@users.noreply.github.com>
2023-02-04 05:08:38 -0500

    wuuuuuuu

---
+++ GameJam2023/Assets/Scripts/Daniel Scripts/Waves.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Waves : MonoBehaviour
+{
+    public float maxX;
+    public float maxY;
+    public float minX;
+    public float minY;
+    public float timeBetweenSpawn;
+    private float spawnTime;
+
+    private void Update()
+    {
+        if(Time.time > spawnTime)
+        {
+            Spawn();
+            spawnTime = Time.time + timeBetweenSpawn;
+        }
+           
+    }
+
+    void Spawn()
+    {
+        float randomX = Random.Range(minX, maxX);
+        float randomY= Random.Range(minY, maxY);
+        Debug.Log("Sig enrea");
+        PlataformaBasicaPool.Instance.Get().transform.position = new Vector2(randomX, randomY);
+
+
+    }
+}
